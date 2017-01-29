0f96d0f7fca45fa534f0dad9f1025ca916408f24
FreezeMan <freeze-man@users.noreply.github.com>
2017-01-29 13:37:59 +0330

    update file and remove test api

--- config.php
+++ config.php
@@ -15,10 +15,7 @@ $admin_pass= '123456';//کلمه رمز بخش مدیریت
 
 
 //site url with / at end of address
-$site_url='http://localhost/asanpardakht3/';//آدرس محل نصب بدون / دراخر نوشته شود
-$api_key='86444d9c-e72e-4323-bf96-d3da90d39ed4';
+$site_url='http://localhost/asanpardakht/';//آدرس محل نصب بدون / دراخر نوشته شود
+$api_key='';
 $order_id=time();
-//'d66e3e74-e86b-4f6a-a114-397559ee262f';// شناسه درگاه درپنل کاربری بخش درگاه این شناسه ذکر شده است
-//'4905cc72-413c-4b8a-beac-7259a2c6b485';
-//'fb50c91b-ef95-4250-92c2-bbca36f3ed1c';
 ?>
